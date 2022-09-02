20ca1a47a9b1dbd4a44862df606f71fc59cfeffe
AndrewZuo01 <59896149+AndrewZuo01@users.noreply.github.com>
2022-09-02 14:59:21 -0500

    Add files via upload
    error on delete trap

---
+++ CamelUp/game.cpp
@@ -0,0 +1,9 @@
+//
+//  game.cpp
+//  CamelUp
+//
+//  Created by andrew on 2022/8/23.
+//
+
+#include "game.hpp"
+
---
+++ CamelUp/game.hpp
@@ -0,0 +1,13 @@
+//
+//  game.hpp
+//  CamelUp
+//
+//  Created by andrew on 2022/8/23.
+//
+
+#ifndef game_hpp
+#define game_hpp
+
+#include <stdio.h>
+
+#endif /* game_hpp */
